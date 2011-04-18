9392d549398c84e89f75aebe35e1aa88c6ef71e8
Ben Atkin <ben@benatkin.com>
2011-04-17 20:12:55 -0600

    read and write README.md (no changes yet)

--- build.js
+++ build.js
@@ -1,4 +1,8 @@
 var fs    = require('fs'),
     sites = require('./sites').sites;
 
+fs.readFile('README.md', function(err, data) {
+  fs.writeFile('README.md', data);
+});
+
 fs.writeFile('sites.json', JSON.stringify(sites, null, 2) + "\n");
