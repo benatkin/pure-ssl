9e9b9e96cc711bf1f7ac5496d2501eb8c3e9a9de
Ben Atkin <ben@benatkin.com>
2011-04-17 20:27:24 -0600

    split and join lines

--- build.js
+++ build.js
@@ -1,8 +1,9 @@
 var fs    = require('fs'),
     sites = require('./sites').sites;
 
-fs.readFile('README.md', function(err, data) {
-  fs.writeFile('README.md', data);
+fs.readFile('README.md', 'utf8', function(err, data) {
+  var lines = data.split("\n");
+  fs.writeFile('README.md', lines.join("\n"));
 });
 
 fs.writeFile('sites.json', JSON.stringify(sites, null, 2) + "\n");
